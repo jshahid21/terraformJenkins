3c3e336ae5dd5f021a5dd50d896e3af6c3e11815
jshahid21 <42783926+jshahid21@users.noreply.github.com>
2019-03-06 15:36:32 -0500

    update provider

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -6,8 +6,16 @@ variable "compartment_ocid" {}
 variable "region" {}
 
 provider "oci" {
-#   auth   = "InstancePrincipal"
-  region = "us-ashburn-1"
+
+}
+
+provider "oci" {
+  auth   = "InstancePrincipal"
+  region = "${var.region}"
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
 }
 
 resource "oci_core_virtual_network" "VCN" {
