6116b3f659333d62d4809fe71fa83b40f3920971
jshahid21 <42783926+jshahid21@users.noreply.github.com>
2019-03-07 14:00:24 -0500

    deploy using Instance Principal

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -8,10 +8,10 @@ variable "region" {}
 provider "oci" {
   auth   = "InstancePrincipal"
   region = "${var.region}"
-  tenancy_ocid = "${var.tenancy_ocid}"
-  user_ocid = "${var.user_ocid}"
-  fingerprint = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
+#   tenancy_ocid = "${var.tenancy_ocid}"
+#   user_ocid = "${var.user_ocid}"
+#   fingerprint = "${var.fingerprint}"
+#   private_key_path = "${var.private_key_path}"
 }
 
 resource "oci_core_virtual_network" "VCN" {
